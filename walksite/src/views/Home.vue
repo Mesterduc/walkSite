23984831ae526251172a5f2f22408427b6d3fff6
Mesterduc <hongduccai@gmail.com>
2020-12-08 03:54:55 +0100

    sd
    sd

---
+++ walksite/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		<section class="table-container">
+			<div class="table">
+				<span class="table_header"> DanDomain</span>
+				<div class="table_col-header">
+					<span >navn</span>
+					<span>antal</span>
+				</div>
+        <div class="table_worker">
+					<span class="table_worker-name">mads</span>
+					<span class="table_worker-antal">100</span>
+				</div>
+        <div class="table_worker">
+					<span class="table_worker-name">asdadasda</span>
+					<span class="table_worker-antal">82</span>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	components: {},
+}
+</script>
