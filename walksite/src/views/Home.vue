35e642e887bcb3c676e2ce054e7c684e05645650
Mesterduc <hongduccai@gmail.com>
2020-12-08 04:57:07 +0100

    d
    asd

--- walksite/src/views/Home.vue
+++ walksite/src/views/Home.vue
@@ -8,7 +8,23 @@
 					<span>antal</span>
 				</div>
         <div class="table_worker">
-					<span class="table_worker-name">mads</span>
+					<span class="table_worker-name">kristine holmskov</span>
+					<span class="table_worker-antal">100</span>
+				</div>
+        <div class="table_worker">
+					<span class="table_worker-name">asdadasda</span>
+					<span class="table_worker-antal">82</span>
+				</div>
+			</div>
+
+      <div class="table">
+				<span class="table_header"> DanDomain</span>
+				<div class="table_col-header">
+					<span >navn</span>
+					<span>antal</span>
+				</div>
+        <div class="table_worker">
+					<span class="table_worker-name">kristine holmskov</span>
 					<span class="table_worker-antal">100</span>
 				</div>
         <div class="table_worker">
